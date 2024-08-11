dc6b561a5b32f123d538fb978b2e8206799d8ede
andyg2 <rehit.it@gmail.com>
2024-08-11 21:58:54 +0800

    Updated to php 7.4 and changed exposed ports to not conflict with existing host servers.

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # docker-compose-php-nginx-mysql-redis
+
 ## How to use
 > docker-compose up -d
+> open http://localhost:8080 and create database test
+> open localhost:88
 
-## In PHP File
+## Notes.
+To avoid conflicts with existing host services:
+  changed the web server port to 88 (./docker-compose.yml:11)
+  Changed the Mqsql port to 3303 (./docker-compose.yml:30)
--- web/index.php
+++ web/index.php
@@ -4,20 +4,34 @@
 // Redis
 $redis = new Redis();
 $redis->connect('redis', 6379);
-echo "Connection to server sucessfully";
+echo "Connection to server sucessfully" . "<br>" . PHP_EOL;
 $redis->set("tutorial-name", "Redis tutorial");
 echo "Stored string in redis:: " . $redis->get("tutorial-name");
-
-
-// MySQL
 
 $servername = 'mysql';
 $username = 'root';
 $password = 'root';
+$port = 3306;
 
-$mysql = new MySQLi($servername, $username, $password, 'wordpress');
+// Connect to the new database
+$mysql = new MySQLi($servername, $username, $password, 'test', $port);
+
+// Check connection
 if ($mysql->connect_errno) {
-  echo "Errno: " . $mysql->connect_errno . "\n";
+  echo '<h3>First connect to <a href="http://localhost:8080/">PHPMyAdmin</a> (root:root) and create the database `test`</h3>';
+  die("Connection to 'test' failed: " . $mysql->connect_error);
 }
-$result = $mysql->query("show databases");
-print_r($result);
+
+// Show databases
+$result = $mysql->query("SHOW DATABASES");
+
+if ($result) {
+  while ($row = $result->fetch_assoc()) {
+    echo $row['Database'] . "<br>" . PHP_EOL;
+  }
+} else {
+  echo "Error fetching databases: " . $mysql->error . "<br>" . PHP_EOL;
+}
+
+// Close connection
+$mysql->close();
